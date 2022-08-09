a699fb6ecaf69eaa0684bf58e9932dfb7a42f971
HardCoreRikkei <longhd2@rikkeisoft.com>
2022-08-09 14:35:04 +0700

    Create Project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Home from './Components/Home';
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/index.js
@@ -0,0 +1,22 @@
+import logo from '../../Asset/image/logo.svg';
+import './style.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <a
+          className="Home-link"
+          href="/."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ReactJS 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
