739f96c37f875c84d09eda9ab371a712ebe33109
longhd2 <longhd2@rikkeisoft.com>
2022-09-21 09:32:30 +0700

    update text in home

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -13,7 +13,7 @@ const Home = () => {
             target="_blank"
             rel="noopener noreferrer"
         >
-            Rekkei 
+            Ho Duc Long  
         </a>
       </header>
     </div>
